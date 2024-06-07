73226e115423236f8355059bedecf506dddd153c
Sait <sait108@proton.me>
2024-06-07 14:37:15 +0200

    feat: update homepage with more content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className='min-h-screen bg-gray-800 text-slate-100'>
-      <div className='container grid grid-cols-[20rem_auto] h-full min-h-screen'>
+      <div className='container grid grid-cols-[21rem_auto] h-full min-h-screen'>
         <div className='border-l border-r border-slate-500 px-8 '>
           <nav className='flex flex-col h-full min-h-screen align-middle fixed'>
             <h1 className='fixed top-0 pt-16 px-2 text-4xl font-bold tracking-wide'>Sait<span className='text-red-300'>'</span>s<br /><span className='font-normal tracking-normal'>Portfolio</span></h1>
             <ul className='space-y-5 flex flex-col pt-16 p-12 justify-center h-full'>
               <li className='before:content-["↠"] before:text-gray-500 before:absolute before:-left-3 before:-translate-x-full before:-translate-y-1/2 relative before:text-2xl before:top-1/2 before:scale-y-125'>
-                <Link className='text-3xl' href={'/about-me'}>
+                <Link className='text-3xl' href={'#about-me'}>
                   About me
                 </Link>
               </li>
               <li className='before:content-["↠"] before:text-gray-500 before:absolute before:-left-3 before:-translate-x-full before:-translate-y-1/2 relative before:text-2xl before:top-1/2 before:scale-y-125'>
-                <Link className='text-3xl' href={''}>
+                <Link className='text-3xl' href={'#side-projects'}>
                   Side Projects
                 </Link>
               </li>
@@ -24,13 +24,16 @@ export default function Home() {
                 </Link>
               </li>
               <li className='ml-6 !mt-2 before:content-["↠"] before:absolute before:-left-3 before:-translate-x-full before:-translate-y-1/2 relative before:text-2xl before:text-gray-500 before:top-1/2 before:scale-y-125'>
-                <Link className='text-2xl' href={'/tic-tac-toe'}>Tic-Tac-Toe</Link>
+                <Link className='text-2xl' href={'/tictactoe'}>Tic-Tac-Toe</Link>
+              </li>
+              <li className='before:content-["↠"] before:text-gray-500 before:absolute before:-left-3 before:-translate-x-full before:-translate-y-1/2 relative before:text-2xl before:top-1/2 before:scale-y-125'>
+                <Link className='text-3xl' href={'#other-projects'}>Other Projects</Link>
               </li>
             </ul>
           </nav>
         </div>
         <div className='px-12 prose prose-invert prose-p:text-lg prose-h1:font-medium prose-h1:text-3xl justify-self-stretch'>
-          <div className='flex min-h-screen'>
+          <div id='about-me' className='flex min-h-screen'>
             <div className='my-auto'>
               <h1>About me.</h1>
               <p><span className='text-red-200 text-xl mr-2'>Hello there!</span> I am Sait. A developer based in Berlin, Germany.</p>
@@ -39,13 +42,27 @@ export default function Home() {
               <p>My main frontend framework I use is <span className='text-cyan-300'>React</span>.</p>
             </div>
           </div>
-          <div className='flex min-h-screen'>
+          <div id='side-projects' className='flex min-h-screen'>
             <div className='my-auto'>
-              <h1>About me.</h1>
-              <p><strong className='text-red-200'>Hello there!</strong> I am Sait. A developer based in Berlin, Germany.</p>
-              <p>Over the duration of my <span className='text-red-300'>3 years</span> of studies <span className='text-red-300'>I have been working as a web developer</span> (working student).</p>
-              <p>I switched to working full time as a frontend developer in <span className='text-cyan-300'>07.2022</span> and collected <u className='text-cyan-300'>17 months of frontend development experience</u> with a great team of seasoned developers and engineers.</p>
+              <h1>Side Projects</h1>
+              <p>I am creating projects in my free time and showcase them here, since it's hard for me to show things that I built for other companies.</p>
+              <p>Here is a list with links:</p>
+              <ul>
+                <li><Link href={'/notes'}>Notes</Link></li>
+                <li><Link href={'/tictactoe'}>Tic-Tac-Toe</Link></li>
+              </ul>
               <p>My main frontend framework I use is <span className='text-cyan-300'>React</span>.</p>
+            </div>
+          </div>
+          <div id="other-projects" className='flex min-h-screen'>
+            <div className='my-auto'>
+              <h1>Other projects</h1>
+              <p>I have worked on several websites and projects. Here are the ones that I can publicly list:</p>
+              <ul>
+                <li><a className='decoration-red-500 underline-offset-4 decoration-2' href="https://beefbusters.de/">Beef Busters</a></li>
+                <li><a className='decoration-blue-400 underline-offset-4 decoration-2' href="https://kh-zahnarztpraxis.de/">KH Zahnarztpraxis</a></li>
+                <li><a className='decoration-cyan-400 underline-offset-4 decoration-2' href="https://can-facilityservices.de/">Can Facility Services</a></li>
+              </ul>
             </div>
           </div>
         </div>
