0d63beada5d0eedb4956a47c58a02f3af5575a38
Sait <sait108@proton.me>
2024-06-14 14:05:03 +0200

    feat: update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,34 +6,34 @@ export default function Home() {
     <main className='min-h-screen bg-gray-800 text-slate-100'>
       <div className='fixed h-1/6 bottom-0 w-screen bg-gradient-to-t from-gray-800/90 to-gray-800/0 pointer-events-none'></div>
       <div className='fixed h-1/6 top-0 w-screen bg-gradient-to-t to-gray-800/90 from-gray-800/0 pointer-events-none'></div>
-      <div className='container grid grid-cols-[21rem_auto] h-full min-h-screen'>
-        <div className='border-l border-r border-slate-500 px-8 '>
-          <nav className='flex flex-col h-full min-h-screen align-middle fixed'>
-            <h1 className='fixed top-0 pt-16 px-2 text-4xl font-bold tracking-wide'>Sait<span className='text-red-300'>&apos;</span>s<br /><span className='font-normal tracking-normal'>Project</span></h1>
-            <ul className='space-y-5 flex flex-col pt-16 p-12 justify-center h-full'>
+      <div className='container grid lg:grid-cols-[19rem_auto] h-full min-h-screen'>
+        <div className='border-l border-r border-slate-500 px-8 lg:px-0'>
+          <nav className='flex lg:flex-col lg:h-full lg:min-h-screen align-middle fixed top-0 left-0 right-0 lg:left-auto lg:right-auto px-5 lg:w-auto justify-between'>
+            <h1 className='fixed top-0 pt-6 lg:pt-16 px-2 text-3xl lg:text-4xl font-bold tracking-wide'>Sait<span className='text-red-300'>&apos;</span>s<br /><span className='font-normal tracking-normal'>Project</span></h1>
+            <ul className='space-y-5 flex flex-col pt-6 lg:pt-16 lg:p-12 justify-center h-full ml-auto'>
               <li className='before:content-["↠"] before:text-gray-500 before:absolute before:-left-3 before:-translate-x-full before:-translate-y-1/2 relative before:text-2xl before:top-1/2 before:scale-y-125'>
-                <Link className='text-3xl opacity-60 hover:opacity-100 transition-opacity' href={'#about-me'}>
+                <Link className='text-2xl opacity-60 hover:opacity-100 transition-opacity' href={'#about-me'}>
                   About me
                 </Link>
               </li>
               <li className='before:content-["↠"] before:text-gray-500 before:absolute before:-left-3 before:-translate-x-full before:-translate-y-1/2 relative before:text-2xl before:top-1/2 before:scale-y-125'>
-                <Link className='text-3xl opacity-60 hover:opacity-100 transition-opacity' href={'#side-projects'}>
+                <Link className='text-2xl opacity-60 hover:opacity-100 transition-opacity' href={'#side-projects'}>
                   Side Projects
                 </Link>
               </li>
               <li className='ml-6 !mt-3 before:content-["🔗"] before:text-gray-500 before:absolute before:-left-3 before:-translate-x-7 before:-translate-y-1/2 relative before:text-2xl before:top-1/2'>
-                <Link className='text-2xl opacity-60 hover:opacity-100 hover:text-blue-400 transition-all' href={'/notes'}>
+                <Link className='text-xl opacity-60 hover:opacity-100 hover:text-blue-400 transition-all' href={'/notes'}>
                   Notes
                 </Link>
               </li>
               <li className='ml-6 !mt-2 before:content-["🔗"] before:absolute before:-left-3 before:-translate-x-7 before:-translate-y-1/2 relative before:text-2xl before:text-gray-500 before:top-1/2'>
-                <Link className='text-2xl opacity-60 hover:opacity-100 hover:text-blue-200 transition-all' href={'/tictactoe'}>Tic-Tac-Toe</Link>
+                <Link className='text-xl opacity-60 hover:opacity-100 hover:text-blue-200 transition-all' href={'/tictactoe'}>Tic-Tac-Toe</Link>
               </li>
               <li className='before:content-["↠"] before:text-gray-500 before:absolute before:-left-3 before:-translate-x-full before:-translate-y-1/2 relative before:text-2xl before:top-1/2 before:scale-y-125'>
-                <Link className='text-3xl opacity-60 hover:opacity-100 transition-opacity' href={'#other-projects'}>Other Projects</Link>
+                <Link className='text-2xl opacity-60 hover:opacity-100 transition-opacity' href={'#other-projects'}>Other Projects</Link>
               </li>
               <li className='before:content-["↠"] before:text-gray-500 before:absolute before:-left-3 before:-translate-x-full before:-translate-y-1/2 relative before:text-2xl before:top-1/2 before:scale-y-125'>
-                <Link className='text-3xl opacity-60 hover:opacity-100 transition-opacity hover:text-yellow-100' href={'#tech-i-use'}>Tech I use</Link>
+                <Link className='text-2xl opacity-60 hover:opacity-100 transition-opacity hover:text-yellow-100' href={'#tech-i-use'}>Tech I use</Link>
               </li>
             </ul>
           </nav>
