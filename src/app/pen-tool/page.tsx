2af66d8860d3f693a11915195b824a7a8389fa18
Sait <sait108@proton.me>
2024-10-30 03:04:49 +0100

    feat: update project

--- src/app/pen-tool/page.tsx
+++ src/app/pen-tool/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import React, { useCallback, useState } from 'react';
 import { Stage, Layer, Circle, Line, Rect } from 'react-konva';
 import Konva from 'konva';
