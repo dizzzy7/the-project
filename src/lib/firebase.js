349d011e85e11147e42d957a87fb393dd30d5c78
Sait <sait108@proton.me>
2024-12-03 13:20:37 +0100

    config: add config for firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCz_r2jjUnlRnmugLOhlRXUHPiSH4ps27Y',
+  authDomain: 'the-project-371ab.firebaseapp.com',
+  projectId: 'the-project-371ab',
+  storageBucket: 'the-project-371ab.firebasestorage.app',
+  messagingSenderId: '400077307150',
+  appId: '1:400077307150:web:e2a6745e80da965112eb5f',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
