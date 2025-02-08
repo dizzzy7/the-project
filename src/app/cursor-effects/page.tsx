9038480d38fc78f1c6baa77f4a957e6c2802824b
Sait <sait108@proton.me>
2025-02-08 14:01:17 +0100

    feat: use new svg flashlight component in CursorEffectsPage

--- src/app/cursor-effects/page.tsx
+++ src/app/cursor-effects/page.tsx
@@ -1,5 +1,6 @@
 
 import MaskImageFlashlight from '@/components/ui/masks/mask-image-flashlight';
+import SVGMaskFlashlight from '@/components/ui/masks/svg-mask-flashlight';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -7,40 +8,34 @@ export const metadata: Metadata = {
   description: 'This page contains cool effects for the mouse cursor',
 };
 
-/**
- * BOOKMARKS, RECENT PAGES/BOOKMARKS, QUICK NOTES
- * WEATHER, BUSSTATIONEN O. FAHRPLAN, TERMINE/FEIERTAGE (Google Calendar)
- */
-
-const CursorEffectsPage = () => {
-  return (
-    <main className='bg-slate-800 h-screen text-white'>
-      <section className='h-full container relative bg-slate-800'>
-        <h1 className='text-2xl pt-8'>Cursor Effects</h1>
-        <div className='my-4'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-        </div>
-        <div className='my-4'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
-        </div>
-      </section>
-      <MaskImageFlashlight />
-    </main>
-  );
-};
+const CursorEffectsPage = () => (
+  <main className='bg-slate-800 h-screen text-white'>
+    <section className='h-full container relative bg-slate-800'>
+      <h1 className='text-2xl pt-8'>Cursor Effects</h1>
+      <div className='my-4'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+      </div>
+      <div className='my-4'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde excepturi dicta quaerat eum maiores. Deserunt, ex. Incidunt assumenda dignissimos nostrum provident praesentium iusto, ratione dicta ducimus hic laboriosam, perspiciatis laborum!</p>
+      </div>
+    </section>
+    {/* <MaskImageFlashlight /> */}
+    <SVGMaskFlashlight />
+  </main>
+);
 
 export default CursorEffectsPage;
